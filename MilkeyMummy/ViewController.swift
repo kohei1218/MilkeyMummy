2605b764d3cad135585470acd453614700806b42
kohei1218 <yandapanda1218@gmail.com>
2018-06-11 14:24:45 +0900

    Initial Commit

---
+++ MilkeyMummy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MilkeyMummy
+//
+//  Created by 齋藤　航平 on 2018/06/11.
+//  Copyright © 2018年 齋藤　航平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
