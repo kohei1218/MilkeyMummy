4992b1b5d69ea476c23866fc27479ed803837ad9
kohei1218 <yandapanda1218@gmail.com>
2018-07-05 01:51:09 +0900

    feat: add chatview

---
+++ MilkeyMummy/ViewControllers/MessageViewController.swift
@@ -0,0 +1,78 @@
+//
+//  MessageViewController.swift
+//  MilkeyMummy
+//
+//  Created by 齋藤　航平 on 2018/07/05.
+//  Copyright © 2018年 齋藤　航平. All rights reserved.
+//
+
+import UIKit
+import JSQMessagesViewController
+
+class MessageViewController: JSQMessagesViewController {
+    
+    // メッセージ内容に関するプロパティ
+    var messages: [JSQMessage]?
+    // 背景画像に関するプロパティ
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    // アバター画像に関するプロパティ
+    var incomingAvatar: JSQMessagesAvatarImage!
+    var outgoingAvatar: JSQMessagesAvatarImage!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Sendボタンが押された時に呼ばれるメソッド
+    override func didPressSend(_ button: UIButton, withMessageText text: String, senderId: String, senderDisplayName: String, date: Date) {
+        
+        //メッセージの送信処理を完了する(画面上にメッセージが表示される)
+        self.finishReceivingMessage(animated: true)
+        
+        //firebaseにデータを送信、保存する
+//        let post1 = ["from": senderId, "name": senderDisplayName, "text":text]
+//        let post1Ref = ref.childByAutoId()
+//        post1Ref.setValue(post1)
+//        self.finishSendingMessage(animated: true)
+        
+        //キーボードを閉じる
+        self.view.endEditing(true)
+    }
+    
+    // アイテムごとに参照するメッセージデータを返す
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, messageDataForItemAt indexPath: IndexPath) -> JSQMessageData {
+        return messages![indexPath.item]
+    }
+    
+    // アイテムごとのMessageBubble(背景)を返す
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, messageBubbleImageDataForItemAt indexPath: IndexPath) -> JSQMessageBubbleImageDataSource {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingBubble
+        }
+        return self.incomingBubble
+    }
+    
+    // アイテムごとにアバター画像を返す
+    override func collectionView(_ collectionView: JSQMessagesCollectionView, avatarImageDataForItemAt indexPath: IndexPath) -> JSQMessageAvatarImageDataSource? {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingAvatar
+        }
+        return self.incomingAvatar
+    }
+    
+    // アイテムの総数を返す
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages!.count
+    }
+
+}
